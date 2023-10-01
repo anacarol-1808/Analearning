8aaae56b978b9f4590b52d96a368773e2f64eec6
anacarol-1808 <anacarol_1808@hotmail.com>
2023-09-30 22:24:41 -0600

    addto Cart  works

--- src/js/product.js
+++ src/js/product.js
@@ -1,9 +1,20 @@
 import { setLocalStorage } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
 
+var items = [];
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
-}
+  const existing = localStorage.getItem("so-cart");
+  const shoppingCart = [];
+  if(existing) {
+    try {
+      shoppingCart.push(...JSON.parse(existing))
+    } catch(err) {console.error("ERROR deserializing cart", err);}
+    }
+    shoppingCart.push(product);
+    localStorage.setItem("so-cart", JSON.stringify(shoppingCart));
+  }
+
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await findProductById(e.target.dataset.id);
